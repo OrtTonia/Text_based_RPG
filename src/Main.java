b10fc5d4c601007c9e26aa9ed4ee0bb0d7045339
Toni <ortenburger.70584@m365.bfwbb-lernen.de>
2025-03-26 08:32:31 +0100

    scanner hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // create view
         GameView gameView = new GameView();
 
+        Scanner scanner = new Scanner(System.in);
 
         // Create the table if not exist for storing characters in the database
-        DBController db = new DBController();
+        DBManager db = new DBManager();
         db.createTableCharacter();
 
         // Display the game logo
         gameView.printWelcomeMessage();
 
-        Game controller = new Game(gameView, db);
+        GameController controller = new GameController(gameView, db, scanner);
         // Start the game loop
         controller.startGame();
     }
